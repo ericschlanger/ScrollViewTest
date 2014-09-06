2c1d72766e728092272a7e7a75373f749c8c53ea
macdoum1 <macdoum1@tcnj.edu>
2014-09-05 21:27:23 -0400

    all the cards

--- ScrollViewTest/CardLocation.m
+++ ScrollViewTest/CardLocation.m
@@ -8,29 +8,29 @@
 
 #import "CardLocation.h"
 
- const CGFloat center1 = 245;
- const CGFloat alpha1 = .3f;
- const CGFloat transform1 = 29/32.0;
+ const CGFloat center0 = 245;
+ const CGFloat alpha0 = .3f;
+ const CGFloat transform0 = 29/32.0;
 
- const CGFloat center2 = 270;
- const CGFloat alpha2 = .5f;
- const CGFloat transform2 = 31/32.f;
+ const CGFloat center1 = 270;
+ const CGFloat alpha1 = .5f;
+ const CGFloat transform1 = 31/32.f;
 
- const CGFloat center3 = 289;
+ const CGFloat center2 = 289;
+ const CGFloat alpha2 = 1.f;
+ const CGFloat transform2 = 1;
+
+ const CGFloat center3 = 777.25;
  const CGFloat alpha3 = 1.f;
  const CGFloat transform3 = 1;
 
- const CGFloat center4 = 500;
- const CGFloat alpha4 = 1.f;
- const CGFloat transform4 = 1;
+ const CGFloat center4 = 762.25;
+ const CGFloat alpha4 = .5f;
+ const CGFloat transform4 = 31/32.f;
 
- const CGFloat center5 = 490;
- const CGFloat alpha5 = .5f;
- const CGFloat transform5 = 31/32.f;
-
- const CGFloat center6 = 480;
- const CGFloat alpha6 = .3f;
- const CGFloat transform6 = 29/32.0;
+ const CGFloat center5 = 740.25;
+ const CGFloat alpha5 = .3f;
+ const CGFloat transform5 = 29/32.0;
 
 
 @implementation CardLocation
@@ -47,17 +47,17 @@
 
     switch (index) {
         case 0:
-            return [CardLocation locationWithCenter:CGPointMake(160, center1) alpha:alpha1 transform:CGAffineTransformMakeScale(transform1, transform1)];
+            return [CardLocation locationWithCenter:CGPointMake(160, center0) alpha:alpha0 transform:CGAffineTransformMakeScale(transform0, transform0)];
         case 1:
-            return [CardLocation locationWithCenter:CGPointMake(160, center2) alpha:alpha2 transform:CGAffineTransformMakeScale(transform2, transform2)];
+            return [CardLocation locationWithCenter:CGPointMake(160, center1) alpha:alpha1 transform:CGAffineTransformMakeScale(transform1, transform1)];
         case 2:
-            return [CardLocation locationWithCenter:CGPointMake(160, center3) alpha:alpha3 transform:CGAffineTransformMakeScale(transform3, transform3)];
+            return [CardLocation locationWithCenter:CGPointMake(160, center2) alpha:alpha2 transform:CGAffineTransformMakeScale(transform2, transform2)];
         case 3:
-            return [CardLocation locationWithCenter:CGPointMake(160, center4) alpha:alpha4 transform:CGAffineTransformMakeScale(transform4, transform4)];
+            return [CardLocation locationWithCenter:CGPointMake(160, center3) alpha:alpha3 transform:CGAffineTransformMakeScale(transform3, transform3)];
         case 4:
-            return [CardLocation locationWithCenter:CGPointMake(160, center5) alpha:alpha5 transform:CGAffineTransformMakeScale(transform5, transform5)];
+            return [CardLocation locationWithCenter:CGPointMake(160, center4) alpha:alpha4 transform:CGAffineTransformMakeScale(transform4, transform4)];
         case 5:
-            return [CardLocation locationWithCenter:CGPointMake(160, center6) alpha:alpha6 transform:CGAffineTransformMakeScale(transform6, transform6)];
+            return [CardLocation locationWithCenter:CGPointMake(160, center5) alpha:alpha5 transform:CGAffineTransformMakeScale(transform5, transform5)];
     }
     return nil;
 }
--- ScrollViewTest/MainViewController.m
+++ ScrollViewTest/MainViewController.m
@@ -57,22 +57,38 @@ typedef NS_ENUM(NSUInteger, CardDeckAnimateDirection) {
 - (void)setupCards {
     self.cardArray = [[NSMutableArray alloc]init];
     
+    CardView *card0 = [[CardView alloc]initWithFrame:CGRectMake(0, 0, 320, 438.5)];
+    card0.delegate = self;
+    [card0 setLocation:[CardLocation locationForIndex:0]];
+    
     CardView *card1 = [[CardView alloc]initWithFrame:CGRectMake(0, 0, 320, 438.5)];
     card1.delegate = self;
-    [self addCard:card1];
-    [card1 setLocation:[CardLocation locationForIndex:0]];
+    [card1 setLocation:[CardLocation locationForIndex:1]];
     
     CardView *card2 = [[CardView alloc]initWithFrame:CGRectMake(0, 0, 320, 438.5)];
     card2.delegate = self;
-    [self addCard:card2];
-    [card2 setLocation:[CardLocation locationForIndex:1]];
+    [card2 setLocation:[CardLocation locationForIndex:2]];
     
     CardView *card3 = [[CardView alloc]initWithFrame:CGRectMake(0, 0, 320, 438.5)];
     card3.delegate = self;
-    [self addCard:card3];
-    [card3 setLocation:[CardLocation locationForIndex:2]];
+    [card3 setLocation:[CardLocation locationForIndex:3]];
     
-    self.lastYOffset = card3.center.y;
+    CardView *card4 = [[CardView alloc]initWithFrame:CGRectMake(0, 0, 320, 438.5)];
+    card4.delegate = self;
+    [card4 setLocation:[CardLocation locationForIndex:4]];
+    
+    CardView *card5 = [[CardView alloc]initWithFrame:CGRectMake(0, 0, 320, 438.5)];
+    card5.delegate = self;
+    [card5 setLocation:[CardLocation locationForIndex:5]];
+    
+    [self addCard:card0];
+    [self addCard:card1];
+    [self addCard:card2];
+    [self addCard:card5];
+    [self addCard:card4];
+    [self addCard:card3];
+    
+    self.lastYOffset = card2.center.y;
 }
 
 - (void)moveCardDeck:(CardDeckAnimateDirection)direction {
@@ -127,17 +143,28 @@ typedef NS_ENUM(NSUInteger, CardDeckAnimateDirection) {
 - (void)cardView:(CardView *)cardView moveWithOffset:(CGFloat)offset withDirection:(ScrollDirection)direction {
     cardView.center = CGPointMake(cardView.center.x, offset + self.lastYOffset);
     
-    CardView *card = self.cardArray[self.currentCardIdx-1];
+    CardView *card1 = self.cardArray[self.currentCardIdx-1];
+    CardView *card0 = self.cardArray[self.currentCardIdx-2];
+    
     CGFloat ratio = offset/kThreshold;
     if(ratio > 1) {
         ratio = 1;
     }
     
-    [card setLocation:[CardLocation locationForStart:1 end:2 distancePercentage:ratio]];
+    [card0 setLocation:[CardLocation locationForStart:0 end:1 distancePercentage:ratio]];
+    [card1 setLocation:[CardLocation locationForStart:1 end:2 distancePercentage:ratio]];
+
 }
 
 - (void)endedDraggingWithCardView:(CardView *)cardView {
+    
+    CGFloat distanceTraveled = abs(cardView.center.y - center2);
+    if(distanceTraveled >= kThreshold) {
+        [self moveCardDeck:CardDeckAnimateDirectionNext];
+    }
+
     self.lastYOffset = cardView.center.y;
+    
 }
 
 
